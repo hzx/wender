bd26c5a0eb8050bdd5965f879f4663917ea78870
Dmitry Dementev <dem@rubear.biz>
2013-02-28 18:35:17 +0400

    divide client ans server, add Image, CheckedToolbar, BlogView, CategorySearchEdit components

--- build_wender_coffee_test.sh
+++ build_wender_coffee_test.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=../mutant:../riffler:$PYTHONPATH
-python ../riffler/setup.py --task="test" --build_path="../build/wender_coffee_test" --test_suite="../wender/wender_coffee/test/test.module" --test_module="../wender/wender_coffee/wender.module"
+python ../riffler/setup.py --task="test" --build_path="../build/wender_coffee_test" --test_suite="../client/wender/wender_coffee/test/test.module" --test_module="../client/wender/wender_coffee/wender.module"
